0a9708f30ee4601c4c03e2bdd6100236126fb4d0
rafaelnmcosta <rafaelnmcosta@outlook.com>
2023-11-05 23:24:46 -0300

    Projeto iniciado

---
+++ scripts/enemy.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var speed:int = 200
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	global_position.x  =global_position.x - (speed * delta)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+func _on_area_entered(area):
+	print("Inimigo morto")
+	queue_free()
